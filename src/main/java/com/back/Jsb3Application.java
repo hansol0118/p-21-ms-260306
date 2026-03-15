de25dbe5b330d69035c4d8411aa0844f93ec9ea6
hansol0118 <wall197944@naver.com>
2026-03-15 18:32:38 +0900

    init

---
+++ src/main/java/com/back/Jsb3Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsb3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jsb3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/Jsb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jsb3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
